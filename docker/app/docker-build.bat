a137f5b874cdd13aab2684131a4bddf883ad4dbc
Arina Goloubitskaya <arinaogo@gmail.com>
2026-03-05 20:36:15 +0300

    added docker

--- docker/app/docker-build.bat
+++ docker/app/docker-build.bat
@@ -4,6 +4,6 @@ setlocal
 set "script_dir=%~dp0"
 for %%i in ("%script_dir%..\\..") do set "repo_root=%%~fi"
 
-docker build -t ai-kpi-backend:local -f "%repo_root%\docker\app\Dockerfile" "%repo_root%"
+docker build -t ai-kpi-be -f "%repo_root%\docker\app\Dockerfile" "%repo_root%"
 
 endlocal
--- docker/app/docker-build.sh
+++ docker/app/docker-build.sh
@@ -3,4 +3,4 @@
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 repo_root="$(cd "$script_dir/../.." && pwd)"
 
-docker build -t ai-kpi-backend:local -f "$repo_root/docker/app/Dockerfile" "$repo_root"
+docker build -t ai-kpi-be -f "$repo_root/docker/app/Dockerfile" "$repo_root"
